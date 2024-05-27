19e0b5a5fe1b8b0130684d872b2d9eeac4b4e716
Alex Braga <contato@alexbraga.com.br>
2024-05-27 17:07:46 -0300

    Add `TrabalhadorController`

---
+++ src/main/java/controller/TrabalhadorController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import context.CalculadoraIRRF;
+import model.Endereco;
+import model.Trabalhador;
+import repository.TrabalhadorRepository;
+import service.CalculoIRRF2024;
+import service.ConsultaCEP;
+import service.ValidadorCPF;
+
+import java.net.URISyntaxException;
+
+public class TrabalhadorController {
+    private final TrabalhadorRepository TRABALHADOR_REPOSITORY;
+
+    public TrabalhadorController() {
+        TRABALHADOR_REPOSITORY = TrabalhadorRepository.getInstance();
+    }
+
+    public void processarTrabalhador(Trabalhador trabalhador) throws URISyntaxException {
+        if (!ValidadorCPF.validarCPF(trabalhador.getCpf())) {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+
+        Endereco endereco = ConsultaCEP.buscar(trabalhador.getCep());
+        trabalhador.setEndereco(endereco.toString());
+        System.out.println("Endereço para o CEP " + trabalhador.getCep() + ": " + endereco);
+
+        CalculadoraIRRF calculadoraIRRF = new CalculadoraIRRF(new CalculoIRRF2024());
+        double irrf = calculadoraIRRF.calcularIRRF(trabalhador);
+        double salarioLiquido = trabalhador.getSalarioBruto() - irrf;
+
+        System.out.println("Salário líquido: " + salarioLiquido);
+
+        TRABALHADOR_REPOSITORY.adicionarOuAtualizar(trabalhador);
+    }
+}
