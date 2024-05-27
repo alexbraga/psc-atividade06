06ff5ff728a6a5128ac799347f5e1b9418c48bf6
Alex Braga <contato@alexbraga.com.br>
2024-05-27 17:13:36 -0300

    Update `Main.java`

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,41 @@
+import controller.TrabalhadorController;
+import model.Trabalhador;
+
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws URISyntaxException {
+        Scanner scanner = new Scanner(System.in);
+
+        // Receber dados do trabalhador
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Salário Bruto: ");
+        double salarioBruto = scanner.nextDouble();
+
+        System.out.print("Desconto INSS: ");
+        double descontoINSS = scanner.nextDouble();
+
+        System.out.print("Número de Dependentes: ");
+        int numeroDependentes = scanner.nextInt();
+
+        System.out.print("Total de Descontos IRRF: ");
+        double totalDescontosIRRF = scanner.nextDouble();
+        scanner.nextLine(); // Consumir a linha restante
+
+        System.out.print("CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.print("CEP: ");
+        String cep = scanner.nextLine();
+
+        Trabalhador trabalhador = new Trabalhador(nome, salarioBruto, descontoINSS, numeroDependentes, totalDescontosIRRF, cpf, cep);
+
+        TrabalhadorController controller = new TrabalhadorController();
+        controller.processarTrabalhador(trabalhador);
+
+        scanner.close();
     }
 }
