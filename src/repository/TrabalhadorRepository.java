e134c5cfdd9371b615765d9d4c3d2d90bee98fe6
Alex Braga <contato@alexbraga.com.br>
2024-05-26 22:30:14 -0300

    Add `TrabalhadorRepository`

---
+++ src/repository/TrabalhadorRepository.java
@@ -0,0 +1,55 @@
+package repository;
+
+import dao.TrabalhadorDAO;
+import model.Trabalhador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrabalhadorRepository {
+    private static TrabalhadorRepository instance;
+    private TrabalhadorDAO trabalhadorDAO;
+    private List<Trabalhador> trabalhadores;
+
+    private TrabalhadorRepository() {
+        trabalhadorDAO = new TrabalhadorDAO();
+        trabalhadorDAO.inicializarArquivo();
+        carregarDados();
+    }
+
+    public static TrabalhadorRepository getInstance() {
+        if (instance == null) {
+            instance = new TrabalhadorRepository();
+        }
+
+        return instance;
+    }
+
+    public void carregarDados() {
+        trabalhadores = trabalhadorDAO.carregarTodos();
+    }
+
+    public void adicionarOuAtualizar(Trabalhador trabalhador) {
+        Trabalhador existente = buscarPorCPF(trabalhador.getCpf());
+        if (existente != null) {
+            trabalhadores.remove(existente);
+        }
+
+        trabalhadores.add(trabalhador);
+        trabalhadorDAO.salvarTodos(trabalhadores);
+    }
+
+    public Trabalhador buscarPorCPF(String cpf) {
+        for (Trabalhador trabalhador : trabalhadores) {
+            if (trabalhador.getCpf().equals(cpf)) {
+                return trabalhador;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Trabalhador> buscarTodos() {
+        return new ArrayList<>(trabalhadores);
+    }
+}
